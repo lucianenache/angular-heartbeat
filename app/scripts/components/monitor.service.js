93a9495b595c70f64c5a0bae582637ba65f778e8
Lucian Enache <enache.lucian@europe.com>
2014-11-28 15:58:45 +0100

    clean-up

--- app/scripts/components/monitor.service.js
+++ app/scripts/components/monitor.service.js
@@ -8,43 +8,23 @@
 	monitorService.$inject = ['$http'];
 
 	function monitorService($http) {
-		var order = 0;
 
 		var service =  {
 			getStatus: getStatus 
-		}
+		};
 
 		return service;
 
-/*
+
 		function getStatus(config) {
 			return $http(config).
-			success(function(data, status, headers, config) {
-    		// this callback will be called asynchronously
-    		// when the response is available
-    		console.log("status for",config.url,status);
-    		return status;
+			success(function(data, response) {
+    			return response;
     	}).
-			error(function(data, status, headers, config) {
-    		// called asynchronously if an error occurs
-    		// or server returns response with an error status
-    		console.log("status for",config.url,status);
-    		return status;
+			error(function(data, response) {
+    			return response;
     	});
 		}
-*/
-		function getStatus(config) {
-		$http.get(config)
-  		.then(function(result) {
-   			 return result;
-		});
-  		}
-		function responseOk(){
 
-		}
-
-		function responseFail(){
-
-		}
 	}
 })();
